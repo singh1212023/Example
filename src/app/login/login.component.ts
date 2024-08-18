d14e9f278508b79165aa4602bf0723133198b2e9
singh1212023 <singh1212023@gmail.com>
2024-08-18 11:00:47 +0530

    gitignorecommit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent {
       userPassword: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]),
     }
   )
-  //new
+  //new changes
   Login(data: any) 
   {
     {
